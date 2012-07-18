5c578cb0d3884ef7f454c555e12ab25d74fda355
Simon Richards <scr52@uclive.ac.nz>
2012-07-18 18:25:14 +1200

    Add nUnit framework core and setup project structure.

---
+++ FakeTest/TestAssert.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FakeTest {
+    [TestClass]
+    public class TestAssert {
+        [TestMethod]
+        public void TestEquals() {
+            Assert.AreEqual(2 * 5, 5 + 5, "wat?");
+        }
+    }
+}
---
+++ msUnit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace msUnit {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length != 1) {
+                Console.WriteLine("Please supply an assembly to run");
+                return;
+            }
+
+            if (!File.Exists(args[0])) {
+                Console.WriteLine("{0} not found.", args[0]);
+            }
+
+            var testRun = AppDomain.CreateDomain("testDomain");
+            testRun.Load(args[0]);
+        }
+    }
+}
