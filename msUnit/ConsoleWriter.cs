45d8be82c8dc3dfd7f17246b17afc406787e06f2
Simon Richards <scr52@uclive.ac.nz>
2012-07-21 23:18:15 +1200

    Improved test output.

--- msUnit/ConsoleWriter.cs
+++ msUnit/ConsoleWriter.cs
@@ -6,17 +6,21 @@ using System.Text;
 namespace msUnit {
     class ConsoleWriter {
 
-        public void AssemblyError(string details) {
-            Console.WriteLine(details);
+        public void AssemblyError(Exception thrown) {
+            Console.WriteLine(thrown.Message);
         }
 
         public void TestCompleted(TestDetails details) {
+            var originalColour = Console.ForegroundColor;
             if (details.Passed) {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("Passed\t" + details.Name);
             } else {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Failed\t" + details.Name);
                 Console.WriteLine(details.Thrown.Message);
             }
+            Console.ForegroundColor = originalColour;
         }
     }
 }
